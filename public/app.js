463ec50f73c1a3874eb35e386f64ce69ae8f69a4
Dane Petersen <thegreatsunra@gmail.com>
2026-03-30 08:54:23 -0500

    Clean up things

--- public/app.js
+++ public/app.js
@@ -9,16 +9,10 @@ function generate(names, adjectives, nouns) {
 function render(names, adjectives, nouns) {
   const list = document.getElementById("hostname-list");
   const items = Array.from({ length: 10 }, (_, i) => {
-    const num = document.createElement("span");
-    num.className = "item-number";
-    num.textContent = i + 1;
-
-    const host = document.createElement("span");
-    host.className = "hostname";
+    const host = document.createElement("code");
     host.textContent = generate(names, adjectives, nouns);
 
     const li = document.createElement("li");
-    li.appendChild(num);
     li.appendChild(host);
     return li;
   });
